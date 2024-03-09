fb36d924f556d2589d62d64a040fcbcb2201bfed
Toxiiiic26 <104536820+Toxiiiic26@users.noreply.github.com>
2024-03-10 01:41:13 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
-OWNER = ["K_O_W_5", "IIUll_l"]
-OWNER_NAME = "ْ𓆩⧛ َ 𝘼َِ𝙁َِ𝙍َِ𝙊َِ𝙊َِ𝙏َِ𝙊ِ ┇ عـ๋͜‏ـۂفــ͡ـࢪتوُ ⧚𓆪"
-BOT_TOKEN = "6368773285:AAHTC8m6vX-6O4N7YfT27HQp0ZSQjWgdnZo"
+OWNER = ["M_Q_ll"]
+OWNER_NAME = "ْ𝕋𝕆𝕏𝕀ℂ ‌♡⁩"
+BOT_TOKEN = "6434332622:AAH-SSyJv9CB-PIwbQQBiRO_FfiUXVLOukU"
 DATABASE = "mongodb+srv://bot_vambir:Al2552001@cluster0.heabj.mongodb.net/vambir_bot?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/UI_VM"
 GROUP = "https://t.me/YR_HX"
